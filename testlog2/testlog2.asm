4fc696408c9bff9a5771c2d9c367e8aa65d5a163
Daud Zoss <daudzoss@gmail.com>
2023-09-26 18:58:32 -0700

    better commenting found a bug

--- testlog2/testlog2.asm
+++ testlog2/testlog2.asm
@@ -12,32 +12,32 @@ start	FUNCALL			;// give frame pointer x a valid initial value
 
 	.include "log2.inc"
 
-putbin:	tya			;
-	pha			;
+putbin:	tya			;void putbin(register uint8_t y) {
+	pha			; uint8_t savey = y;
 	ldy	#8		;
--	rol @w	V0LOCAL		;
-	lda	#"0"		;
-	adc	#0		;
-	jsr	$ffd2		;
+-	rol @w	V0LOCAL		; for (y = 8; y; y--) {
+	lda	#"0"		;  uint8_t c = savey & 0x80;
+	adc	#0		;  savey <<= 1;
+	jsr	$ffd2		;  putchar(c ? '1' : '0');
 	dey			;
-	bne	-		;
+	bne	-		; }
 	POPVARS			;
-	rts			;
+	rts			;} // putbin()
 
-putnyb:	and	#$0f		;
-	bne	+		;
-	bcc	+		;
-	lda	#" "		;
-	bcs	putnyb2		;
-+	ora	#"0"		;
+putnyb:	and	#$0f		;void putnyb(register uint8_t a, register uint8_t c) {
+	bne	+		; a &= 0x0f;
+	bcc	+		; if ((a == 0) && c) { // omit leading 0
+	lda	#" "		;   putchar(' ');
+	bcs	putnyb2		; } else {
++	ora	#"0"		;  a |= '0';
 	cmp	#1+"9"		;
-	bcc	putnyb2		;
+	bcc	putnyb2		;  
 	clc			;
-	adc	#"a"-"9"-1	;
-putnyb2	jsr	$ffd2		;
-	rts			;
+	adc	#"a"-"9"-1	;  putchar ((a <= '9') ? a : a + '9' - 1);
+putnyb2	jsr	$ffd2		; }
+	rts			;} // putnyb()
 
-puthex:	tya			;
+puthex:	tya			;void puthex(register uint8_t y, register uint8_t c) {
 	pha			;
 	tya			;
 	php			;
@@ -46,9 +46,10 @@ puthex:	tya			;
 	lsr			;
 	lsr			;
 	plp			;
-	jsr	putnyb		;
+	jsr	putnyb		; putnyb(a >> 4, c);
 	pla			;
-	jmp	putnyb		;
+	clc			; putnyb(a, 0);
+	jmp	putnyb		;} // puthex()
 
 main:	nop	
 	
